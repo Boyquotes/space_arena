0f396462475434ea6f674c888008b8f28468b125
Habib Syuhada <habibsumedang@gmail.com>
2022-02-07 05:52:51 +0700

    .

---
+++ Objects/Bullet.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export var speed = 750
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position += transform.x * speed * delta
+
+
+func _on_Timer_timeout():
+	queue_free()
---
+++ Objects/ShipWithTrail.gd
@@ -0,0 +1,59 @@
+extends KinematicBody2D
+
+export (int) var accel = 200
+export (int) var max_speed = 1000
+export (float) var rotation_speed = 1.5
+export (PackedScene) var Bullet
+
+var velocity = Vector2()
+var rotation_dir = 0
+var speed = 0
+var shoot = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(delta):
+	get_input()
+	velocity = Vector2(speed, 0).rotated(rotation)
+	rotation += rotation_dir * rotation_speed * delta
+	velocity = move_and_slide(velocity)
+
+func get_input():
+	rotation_dir = 0
+	velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		rotation_dir += 1
+	if Input.is_action_pressed("ui_left"):
+		rotation_dir -= 1
+	if Input.is_action_just_released("ui_down"):
+		#velocity = Vector2(-speed, 0).rotated(rotation)
+		speed -= accel
+		if speed < 0:
+			speed = 0
+	if Input.is_action_just_released("ui_up"):
+		#velocity = Vector2(speed, 0).rotated(rotation)
+		speed += accel 
+		if speed > max_speed:
+			speed = max_speed
+	#if Input.is_action_just_pressed("ui_select"):
+		#shoot()
+
+func shoot():
+	var b = Bullet.instance()
+##	add_child(b)
+##	b.transform = $Muzzle.transform
+	owner.add_child(b)
+	b.transform = $Muzzle.global_transform
+	b.rotation_degrees = b.rotation_degrees + rand_range(-2, 2)
+
+
+func _on_Area2D_area_entered(area):
+	print(area.get_parent().name)
+	shoot = true
+
+
+func _on_Area2D_area_exited(area):
+	shoot = false
